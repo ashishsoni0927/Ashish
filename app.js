cfc72da66b16da31c188164f72e3283267453ff5
ashishsoni0927 <73069751+ashishsoni0927@users.noreply.github.com>
2022-10-10 15:31:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Ashish Soni!');
+  res.send('This is test app!');
 });
 
 app.listen(config.port, () => {
